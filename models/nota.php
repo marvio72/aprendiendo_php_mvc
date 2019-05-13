f9c55ae58c16b835f408ff585078e37a7ce51fcb
Marco Ruvalcaba <desarrollo@oversistemas.com>
2019-05-12 23:04:04 -0500

    Objeto MySQLi_2

--- models/nota.php
+++ models/nota.php
@@ -2,9 +2,13 @@
 
 require_once 'ModeloBase.php';
 
-class Nota extends ModeloBase {
+class Nota extends ModeloBase{
     public $nombre;
     public $contenido;
+
+    public function __construct(){
+        parent::__construct();
+    }
 
     public function getNombre()
     {
